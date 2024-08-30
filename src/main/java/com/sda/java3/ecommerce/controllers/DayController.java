61dfe57a36412687da71a6ab8001cd4ce3f86de9
yug-soni-diatoz <yug.soni@diatoz.com>
2024-08-30 18:51:29 +0530

    latest java error resolved

--- src/main/java/com/sda/java3/ecommerce/controllers/DayController.java
+++ src/main/java/com/sda/java3/ecommerce/controllers/DayController.java
@@ -1,6 +1,6 @@
 package com.sda.java3.ecommerce.controllers;
 
-import com.example.dayservice.service.DayService;
+import com.sda.java3.ecommerce.services.DayService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/sda/java3/ecommerce/domains/ECommerceSystem.java
+++ src/main/java/com/sda/java3/ecommerce/domains/ECommerceSystem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ECommerceSystem {
 
@@ -59,7 +60,7 @@ public class ECommerceSystem {
         public String toJson() {
             return "{\n" +
                    "  \"name\": \"" + name + "\",\n" +
-                   "  \"orders\": " + orders.stream().map(Order::toJson).toList() + "\n" +
+                   "  \"orders\": " + orders.stream().map(Order::toJson).collect(Collectors.toList()) + "\n" +
                    "}";
         }
     }
