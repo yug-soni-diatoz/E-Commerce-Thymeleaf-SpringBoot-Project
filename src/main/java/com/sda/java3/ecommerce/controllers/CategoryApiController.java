de9569936f36816afcafce700dbd09ce96ccba6e
TEBDEX <tebdex@tebdex.com>
2024-08-30 13:23:17 +0000

    Apply tebdex transformation

--- src/main/java/com/sda/java3/ecommerce/controllers/CategoryApiController.java
+++ src/main/java/com/sda/java3/ecommerce/controllers/CategoryApiController.java
@@ -5,7 +5,6 @@ import com.sda.java3.ecommerce.services.category.CategoryService;
 import com.sda.java3.ecommerce.services.category.SaveCategoryRequest;
 import com.sda.java3.ecommerce.services.product.SaveProductRequest;
 import lombok.AllArgsConstructor;
-import lombok.var;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
